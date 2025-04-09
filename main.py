ff6f6027c2aa382fc83a1f6b4a49bb905afce2a6
Arissa <arissakohata@gmail.com>
2025-04-09 20:12:59 -0300

    inicio

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+tela = pygame.display.set_mode(size=(600,480))
+
+# Necessario para deixar a tela aberta e porder fechar quando quiser
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
