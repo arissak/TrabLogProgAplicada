fda0f603009f233e05c7e256b1037da528201598
Arissa <arissakohata@gmail.com>
2025-04-13 09:20:04 -0300

    dp

--- codes/DB.py
+++ codes/DB.py
@@ -3,22 +3,21 @@ import sqlite3
 
 class DB:
     def __init__(self, db_name:str):
-        self.db_name: db_name
+        self.db_name = db_name
         self.connection = sqlite3.connect(db_name)
         self.connection.execute('''
                                 CREATE TABLE IF NOT EXISTS dados(
                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                                 name TEXT NOT NULL, 
-                                gender TEXT NOT NULL, 
-                                date TEXT NOT NULL
+                                gender TEXT NOT NULL)
                                 ''')
 
-    def save(self):
-        self.connection.execute('INSERT INTO dados(name, gender, date) VALUES(:name,:gender,:date)')
+    def save(self,name, gender):
+        self.connection.execute('INSERT INTO dados(name, gender) VALUES(:name,:gender)', {'name': name, 'gender': gender})
         self.connection.commit()
 
     def show(self):
-        return self.connection.execute('SELECT nome FROM dados ORDER BY id DESC LIMIT 1').fetchall()
+        return self.connection.execute('SELECT name FROM dados ORDER BY id DESC LIMIT 1').fetchall()
 
     def close(self):
         return self.connection.close()
--- codes/Registro.py
+++ codes/Registro.py
@@ -1,5 +1,6 @@
+
 import pygame
-from pygame import Surface, Rect
+from pygame import Surface, Rect, K_RETURN, K_TAB, K_BACKSPACE
 from pygame.font import Font
 
 from codes.DB import DB
@@ -14,24 +15,30 @@ class Registro:
         self.surf = pygame.image.load('./asset/2op4.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
-    def save(self, menu_return: str):
-        db = DB('DB_nome'')
-
+        self.nome = ""
+        self.genero = ""
+        self.campo_ativo = "nome"
 
     def run(self):
 
         pygame.mixer_music.load('./asset/som.wav')
         pygame.mixer_music.play(-1)
         voltar_rect = None
+        db = DB('DB_nome.db')
 
         while True:
             self.tela.fill((0, 0, 0))  # Limpa a tela com preto
             self.tela.blit(source=self.surf, dest=self.rect)
 
             voltar_rect = self.menu_text(20, "<-Menu", (255, 255, 255), (40, 30))
+            mutar_rect = self.menu_text(20, "<-Mutar", (255, 255, 255), (40, 70))
 
             self.menu_text(50, "Registro", (160, 32, 240), (288, 90))
-            self.menu_text(30, "Apelido", (255, 255, 255), (288, 120))
+            #self.menu_text(30, "Apelido", (255, 255, 255), (288, 120))
+            self.menu_text(30, "Apelido", (255, 255, 255), (288, 150))
+            self.menu_text(30, "Genero", (255, 255, 255), (288, 220))
+            self.menu_text(25, self.nome + ("|" if self.campo_ativo == "nome" else ""), (255, 255, 0), (288, 180))
+            self.menu_text(25, self.genero + ("|" if self.campo_ativo == "genero" else ""), (255, 255, 0), (288, 250))
 
             pygame.display.flip()
 
@@ -39,11 +46,42 @@ class Registro:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit() #fechar
+
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:  # verifica se o clique e no botao esquerdo
-                        mouse_pos = pygame.mouse.get_pos()  # pega a posicao do botao
-                        if voltar_rect.collidepoint(mouse_pos):  # verifica se o clique foi no botao
-                            return  # faz o botao retornar ao Menu
+                    if event.button == 1:  #verifica se o clique e no botao esquerdo
+                        mouse_pos = pygame.mouse.get_pos()  #pega a posicao do botao
+                        if voltar_rect.collidepoint(mouse_pos):  #verifica se o clique foi no botao
+                            return  #faz o botao retornar ao Menu
+                        if mutar_rect.collidepoint(mouse_pos):
+                            pygame.mixer_music.stop()
+
+                if event.type == pygame.KEYDOWN:
+                        if event.key == K_TAB:
+                            # Alterna entre os campos para escrever genero e nome
+                            self.campo_ativo = "genero" if self.campo_ativo == "nome" else "nome"
+
+                        elif event.key == K_RETURN:
+                            if self.nome.strip() and self.genero.strip():
+                                db.save(self.nome, self.genero)
+                                self.nome = ""
+                                self.genero = "" #ao apertar enter salva no BD
+                            else:
+                                print("Preencha todos os campos.")
+
+                        elif event.key == K_BACKSPACE:
+                            if self.campo_ativo == "nome":
+                                self.nome = self.nome[:-1]
+                            else:
+                                self.genero = self.genero[:-1] #apaga o ultimo caracter se der backspace
+
+                        else:
+                            char = event.unicode
+                            if char.isprintable():
+                                if self.campo_ativo == "nome":
+                                    self.nome += char
+                                else:
+                                    self.genero += char  #permite a escrita
+
 
     def menu_text(self,text_size:int,text:str,text_color:tuple,text_center_pos:tuple):
         text_font: Font = pygame.font.SysFont(name='arial',size=text_size)
