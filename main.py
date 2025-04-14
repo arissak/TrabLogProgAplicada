fb94d5634ce59c16dad773c6b5ce76c319302a59
Arissa <arissakohata@gmail.com>
2025-04-13 21:15:44 -0300

    dp

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import os
+import sys
+import pygame
 from codes.Game import Game
+
+dirpath = os.getcwd()
+sys.path.append(dirpath)
+if getattr(sys, "frozen", False):
+    os.chdir(sys._MEIPASS)
 
 game = Game()
 game.run()
